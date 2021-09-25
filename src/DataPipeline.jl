4e2764af4f9efd35902a6e647c36c251641b1c39
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-09-25 21:48:17 +0100

    internal functions

--- src/DataPipeline.jl
+++ src/DataPipeline.jl
@@ -40,11 +40,6 @@ const DF_MR_SUB_SCR_DESC = "Submission script."
 const FILE_SR_STEM = "file://"
 
 include("core.jl")
-export convert_query
-export http_get_json, get_entry, get_url, get_id
-export extract_id
-export check_exists
-export http_post_data
 export get_file_hash, get_access_token
 
 include("api.jl")
