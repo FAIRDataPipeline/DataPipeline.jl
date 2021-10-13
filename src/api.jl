94fdac4566c3e69ffaab511e24062c9a2689d4f8
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-10-13 17:24:04 +0100

    fix

--- src/api.jl
+++ src/api.jl
@@ -12,7 +12,7 @@ function initialise(config_file::String, submission_script::String)
    
     # Register datastore
     datastore = config["run_metadata"]["write_data_store"]
-    datastore_obj_url = _geturl("storage_root", Dict("root" => datastore))
+    datastore_obj_url = _postentry("storage_root", Dict("root" => datastore))
 
     # Register config file
     config_obj_uri = _registerobject(config_file, datastore, "Working config file")
