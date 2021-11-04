97bdfd0d03bcbb0bba9b238faeb8ac881e9c60c0
Ryan J Field <57794045+RyanJField@users.noreply.github.com>
2021-11-04 03:57:33 +0000

    Update seirs_sim.jl
    Problem with display driver trying to connect to the display; Set display to nul using GKSwstype

--- examples/fdp/seirs_sim.jl
+++ examples/fdp/seirs_sim.jl
@@ -32,6 +32,7 @@ initial_state = Dict("S" => 0.999, "E" => 0.001, "I" => 0, "R" => 0)
 results = modelseirs(initial_state, timesteps, years, alpha, beta, 
                                   inv_gamma, inv_omega, inv_mu, inv_sigma);
 
+ENV["GKSwstype"]="100"
 g = plotseirs(results);
 
 # Save outputs to data store
@@ -39,7 +40,7 @@ path = link_write!(handle, "model_output")
 CSV.write(path, results)
 
 path = link_write!(handle, "figure")
-savefig(g, path);
+savefig(g, path)
 
 # Register code run in local registry
 finalise(handle)
