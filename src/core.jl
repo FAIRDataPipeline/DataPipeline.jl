c407f9d77507ccfd6e13ef1a6ca6ecf32edeb3b4
Ryan Field <RyanJField@users.noreply.github.com>
2022-03-12 04:09:04 +0000

    Debug Query

--- src/core.jl
+++ src/core.jl
@@ -62,6 +62,7 @@ function _convertquery(query::Dict)
     url = "?"
 
     for (key, value) in query
+        print("converting query value: ", value)
         if isa(value, Bool)
             tmp = value
         elseif key == "root" && isnothing(match(r".*://.*", value))
