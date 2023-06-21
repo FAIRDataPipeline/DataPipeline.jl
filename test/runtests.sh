b1648ea90cbd4a6b16072fa5eb93aba754fcdaef
richardreeve <richard.reeve@glasgow.ac.uk>
2023-06-21 11:44:12 +0100

    Fixing paths in testing?

--- test/runtests.sh
+++ test/runtests.sh
@@ -1,11 +1,27 @@
-source ../.venv/$ACTIVATE_DIR/activate
-fair registry install
-fair registry start
-fair init --ci
-fair pull --debug ../examples/fdp/SEIRSconfig.yaml
-fair run --debug --dirty ../examples/fdp/SEIRSconfig.yaml
+#!/bin/bash
 
-value=$@
-sed -e "s,\$PWD,$PWD," -e "s,\$TEST_SCRIPT,$value," pre_config.yaml > config.yaml
-fair run --debug --dirty config.yaml
-rm -f config.yaml
+if [ "$WORKSPACE" = "" ]; then
+  if echo $PWD | grep -qe "test$"; then
+    export WORKSPACE=$PWD/..
+    export TEST_DIR=$PWD
+  else
+    export WORKSPACE=. # Don't really know where the workspace is!
+    export PWD=$WORKSPACE/test
+  fi
+fi
+
+if [ "$ACTIVATE_DIR" = "" ]; then
+  export ACTIVATE_DIR=bin
+fi
+
+echo source $WORKSPACE/.venv/$ACTIVATE_DIR/activate
+echo fair registry install
+echo fair registry start
+echo fair init --ci
+echo fair pull --debug ../examples/fdp/SEIRSconfig.yaml
+echo fair run --debug --dirty ../examples/fdp/SEIRSconfig.yaml
+
+TEST_SCRIPT=$@
+echo sed -e "s,\$TEST_SCRIPT,$TEST_SCRIPT," $TEST_DIR/pre_config.yaml > $TEST_DIR/config.yaml
+echo fair run --debug --dirty config.yaml
+echo rm -f config.yaml
