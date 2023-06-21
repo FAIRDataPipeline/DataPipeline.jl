c6b59a73c2a467eb911597a3797ab7a73a5156da
richardreeve <richard.reeve@glasgow.ac.uk>
2023-06-21 10:51:46 +0100

    Set directory correctly

--- test/runtests.sh
+++ test/runtests.sh
@@ -6,6 +6,6 @@ fair pull --debug ../examples/fdp/SEIRSconfig.yaml
 fair run --debug --dirty ../examples/fdp/SEIRSconfig.yaml
 
 value=$@
-sed -e "s,TEST_SCRIPT,$value," pre_config.yaml > config.yaml
+sed -e "s,\$PWD,$PWD," -e "s,\$TEST_SCRIPT,$value," pre_config.yaml > config.yaml
 fair run --debug --dirty config.yaml
 rm -f config.yaml
