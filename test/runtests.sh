d5cede4f13e28444ec8d4a4a529b0a33d51caa48
richardreeve <richard.reeve@glasgow.ac.uk>
2023-06-21 11:54:39 +0100

    Run tests, don't print them!

--- test/runtests.sh
+++ test/runtests.sh
@@ -14,14 +14,14 @@ if [ "$ACTIVATE_DIR" = "" ]; then
   export ACTIVATE_DIR=bin
 fi
 
-echo source $WORKSPACE/.venv/$ACTIVATE_DIR/activate
-echo fair registry install
-echo fair registry start
-echo fair init --ci
-echo fair pull --debug ../examples/fdp/SEIRSconfig.yaml
-echo fair run --debug --dirty ../examples/fdp/SEIRSconfig.yaml
+source $WORKSPACE/.venv/$ACTIVATE_DIR/activate
+fair registry install
+fair registry start
+fair init --ci
+fair pull --debug $WORKSPACE/examples/fdp/SEIRSconfig.yaml
+fair run --debug --dirty $WORKSPACE/examples/fdp/SEIRSconfig.yaml
 
 TEST_SCRIPT=$@
-echo sed -e "s,\$TEST_SCRIPT,$TEST_SCRIPT," $TEST_DIR/pre_config.yaml > $TEST_DIR/config.yaml
-echo fair run --debug --dirty config.yaml
-echo rm -f config.yaml
+sed -e "s,\$TEST_SCRIPT,$TEST_SCRIPT," $TEST_DIR/pre_config.yaml > $TEST_DIR/config.yaml
+fair run --debug --dirty config.yaml
+rm -f config.yaml
