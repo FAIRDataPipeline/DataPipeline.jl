3e7b460f167b107b95ecedbd6f046fb60820ba0c
richardreeve <richard.reeve@glasgow.ac.uk>
2023-06-21 15:46:15 +0100

    Fix escaping of arguments

--- test/runtests.sh
+++ test/runtests.sh
@@ -12,6 +12,9 @@ else
   export TEST_DIR=$WORKSPACE/test
 fi
 
+echo TEST_DIR: $TEST_DIR
+echo WORKSPACE: $WORKSPACE
+
 if [ "$ACTIVATE_DIR" = "" ]; then
   export ACTIVATE_DIR=bin
 fi
@@ -20,12 +23,14 @@ source $WORKSPACE/.venv/$ACTIVATE_DIR/activate
 fair registry install
 fair registry start
 fair init --ci
-fair pull --debug $WORKSPACE/examples/fdp/SEIRSconfig.yaml
-fair run --debug --dirty $WORKSPACE/examples/fdp/SEIRSconfig.yaml
+fair pull --local $WORKSPACE/examples/fdp/SEIRSconfig.yaml
+fair run --dirty --local $WORKSPACE/examples/fdp/SEIRSconfig.yaml
 
-TEST_SCRIPT=$@
+TEST_SCRIPT="$(printf ' %q' "$@")"
+echo Test: "$TEST_SCRIPT"
 ESCAPED_SCRIPT=$(printf '%s\n' "$TEST_SCRIPT" | sed -e 's/[\,&]/\\&/g')
+echo Escaped test: "$ESCAPED_SCRIPT"
 sed -e "s,\$TEST_SCRIPT,$ESCAPED_SCRIPT," $TEST_DIR/pre_config.yaml > $TEST_DIR/config.yaml
 cat $TEST_DIR/config.yaml
-fair run --debug --dirty $TEST_DIR/config.yaml
-rm -f config.yaml
+fair run --dirty --local --debug $TEST_DIR/config.yaml
+rm -f $TEST_DIR/config.yaml
